a246305bdc1d949f4eeb08ee595e5d4bdabbe633
Tox- <gau.vui97@hotmail.com>
2022-02-18 23:10:28 +0100

    Initial commit

---
+++ src/main/java/fr/ocr/p5_safetynetalerts/P5SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package fr.ocr.p5_safetynetalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5SafetyNetAlertsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P5SafetyNetAlertsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ocr/p5_safetynetalerts/P5SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ocr.p5_safetynetalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P5SafetyNetAlertsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
